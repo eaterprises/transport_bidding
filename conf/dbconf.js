5da72b472d1284abbe82e4b04cbd4144de4d2f10
Timothy Lim <limshenghwee@gmail.com>
2014-02-23 11:40:38 +1100

    change config for heroku use

--- conf/dbconf.js
+++ conf/dbconf.js
@@ -1,15 +1,19 @@
 var conf = {};
-console.log(process.env.MONGO_PWD);
-conf = { user : 'dev',
+// conf = { user : 'dev',
+//           pwd : 'rhokpassword',
+//           url : 'ds053688.mongolab.com',
+//           port: '53688',
+//           db: 'dev-food-transport' };
+conf = { user : process.env.MONGO_USER,
           pwd : process.env.MONGO_PWD,
-          url : 'ds039058.mongolab.com',
-          port: '39058',
+          url : process.env.MONGO_URL,
+          port: process.env.MONGO_PORT,
           db: 'dev-food-transport' };
 
 //mongolab config string
-//conf.mongoUrl = conf.user + ':' + conf.pwd +
-//  '@' + conf.url + ':' + conf.port + '/' + conf.db;
+conf.mongoUrl = conf.user + ':' + conf.pwd + '@' + conf.url + ':' + conf.port + '/' + conf.db;
+
 
 //local instance
-conf.mongoUrl = "localhost/transport-bidding";
+//conf.mongoUrl = "localhost/transport-bidding";
 module.exports = conf;
